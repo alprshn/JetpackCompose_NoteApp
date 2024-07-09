eaf4ce6015a337f607008e15a101d12c8e026911
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-09 14:39:13 +0300

    Added: Color

--- app/src/main/java/com/example/jetpackcompose_noteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/example/jetpackcompose_noteapp/feature_note/domain/model/Note.kt
@@ -1,2 +1,17 @@
 package com.example.jetpackcompose_noteapp.feature_note.domain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf("")
+    }
+}
--- app/src/main/java/com/example/jetpackcompose_noteapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/jetpackcompose_noteapp/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val RedOrange = Color(0xFFFF4500) // Orangered
+val LightGreen = Color(0xFF90EE90) // Light green
+val Violet = Color(0xFFEE82EE) // Violet
+val BabyBlue = Color(0xFF89CFF0) // Baby blue
+val RedPink = Color(0xFFFF69B4) // Hot pink
+
